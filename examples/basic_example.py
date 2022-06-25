6a646c68ea7f0eba7d36d114a5112359de4aeee7
hzappen <hendrik@zappen.net>
2022-06-25 20:45:16 +0200

    Rewrite example using functions

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,7 +1,7 @@
 from solid import *
 import viewscad
 from solid.extensions.bosl2 import *
-from elements.perforatedflatbar import PerforatedStraightBar,STD_HEIGHT,STD_HOLE_DISTANCE
+from elements.perforatedflatbar import getPerforatedStraightBar,STD_HEIGHT,STD_HOLE_DISTANCE
 
 from typing import Any, TYPE_CHECKING
 
@@ -9,26 +9,20 @@ if TYPE_CHECKING:
     metric_screws: OpenSCADObject = None
 
 def two_crossed_bars(render: bool=True):
-    myobj1 = PerforatedStraightBar(60)
-    myobj2 = PerforatedStraightBar(70)
-    print(myobj1.getHolePositions(2))
-    myobj1.getSCADobj().rotate(0,0,90).translate([0,-1*STD_HOLE_DISTANCE/2,STD_HEIGHT])
-    print(myobj1.getHolePositions(2))
+    myobj1 = getPerforatedStraightBar(60)
+    myobj2 = getPerforatedStraightBar(70)
+
+    myobj1 = myobj1.rotate(0,0,90).translate([0,-1*STD_HOLE_DISTANCE,STD_HEIGHT])
+
     bolt = metric_screws.metric_bolt(size=5, headtype='hex', l=10).translate([0,0,2*STD_HEIGHT])
     nut = metric_screws.metric_nut(size=5, hole=True, pitch=1.5, details=False, center=True).translate([0,0,-10+2*STD_HEIGHT])
 
-    assembly = myobj1.getSCADobj()+myobj2.getSCADobj()+bolt+nut
+    assembly = myobj1 + myobj2 + bolt + nut
     if render == True:
         r=viewscad.Renderer()
         r.render(assembly)
     
     return assembly
 
-def test_scadchild():
-    myobj1 = PerforatedStraightBar(60).translate([1,1,1])
-    r=viewscad.Renderer()
-    r.render(myobj1)
-    return myobj1
-    
 
 
--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from solid import *
 from solid.extensions.bosl2 import *
 import viewscad
 
-#assembly = basic_example.two_crossed_bars(render=True)
-
-#assembly.save_as_scad()
-
-#thick = metric_screws.get_metric_nut_thickness()
-#print(thick)
+assembly = basic_example.two_crossed_bars(render=True)
 
 
 
-test = basic_example.test_scadchild()
